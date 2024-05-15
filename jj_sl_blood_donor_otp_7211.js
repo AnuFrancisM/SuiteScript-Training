a934aae11e378b2e5f3aa1d4e8f4e1ef546bc01d
AnuFrancisM <167502745+AnuFrancisM@users.noreply.github.com>
2024-05-15 09:18:31 +0530

    jj_sl_blood_donor_otp_7211.js

--- jj_sl_blood_donor_otp_7211.js
+++ jj_sl_blood_donor_otp_7211.js
@@ -22,40 +22,29 @@ define(['N/record', 'N/ui/serverWidget'],
                 var form =  serverWidget.createForm({
                     title : "Blood Donor Details"
                 });
-               
-               /*
-                var container = form.addFieldGroup({
-                    id : 'Blood_donor_container',
-                    label : 'Blood Donor Details'
-                });
-                */
  
                 form.addField({
                     id : 'custpage_jj_fname_otp_7211',
                     type: serverWidget.FieldType.TEXT,
-                    label : 'First Name',
-                   // container : 'donorInfo'
+                    label : 'First Name'
                 });
  
                 form.addField({
                     id : 'custpage_jj_lname_otp_7211',
                     type: serverWidget.FieldType.TEXT,
-                    label : 'Last Name',
-                   // container : 'donorInfo'
+                    label : 'Last Name'
                 });
                 form.addField({
                     id : 'custpage_jj_gender_otp_7211',
                     type: serverWidget.FieldType.SELECT,
                     label : 'Gender',
-                    source:'422',
-                   // container : 'donorInfo'
+                    source:'422'
                 });
  
                 form.addField({
                     id : 'custpage_jj_phone_otp_7211',
                     type: serverWidget.FieldType.PHONE,
-                    label : 'Phone Number',
-                  //  container : 'donorInfo'
+                    label : 'Phone Number'
                 });
  
                 form.addField({
@@ -75,7 +64,6 @@ define(['N/record', 'N/ui/serverWidget'],
                 });
                 scriptContext.response.writePage(form);
             } else {
-               
 
                 let fname = scriptContext.request.parameters.custpage_jj_fname_otp_7211;
                 let lname = scriptContext.request.parameters.custpage_jj_lname_otp_7211;
@@ -115,28 +103,14 @@ define(['N/record', 'N/ui/serverWidget'],
                 value : lddate
             });
 
-
-           
             var recordId =  customrecord.save({ ignoreMandatoryFields: false, enableSourcing: true });;
             scriptContext.response.write('Record Created with internal Id: '+ recordId);
             log.debug("customer recordId:", + recordId);
             return recordId;
-            
-            // var recordId = customerecord.save();
-            // log.debug("customer recordId:", + recordId);
-           
-            //    let patientRecord = createCustom(patientName, age, Address, Sex);
-            //    log.debug(patientRecord);
-           
         }
-           
- 
-        
         }catch(error){
             log.error(error.message)
         }
     }
-
         return {onRequest}
-
     });
